c4605faa21333dade0865d0c6019f63137f71228
kalifa <906326315@qq.com>
2018-01-04 14:17:08 +0800

    该标签下的前800本书

--- getAuthorsCSV.py
+++ getAuthorsCSV.py
@@ -14,7 +14,7 @@ def get_booklist(author_id):
     url = 'https://book.douban.com/author/'+str(author_id)+'/books?sortby=collect&format=pic'
     header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0'}
     req = urllib.request.Request(url,headers=header)
-    print('req successfully')  
+    #print('req successfully')  
     try: 
         html = urllib.request.urlopen(req,timeout=30).read()
         soup = BeautifulSoup(html,'lxml')
@@ -43,11 +43,7 @@ def get_author(author_id):
     #header = {'Host':'book.douban.com','User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
     header = {'Host': 'book.douban.com',
 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0',
-'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Cookie': 'bid=9-MVpTyNO1o; __utma=30149280.1544947207.1513732889.1514340780.1514359852.7; __utmz=30149280.1513732889.1.1.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; _pk_id.100001.3ac3=bab8cd32029ce905.1514213119.6.1514362223.1514342687.; __utma=81379588.1915505992.1514213121.1514340780.1514359852.6; __utmz=81379588.1514213121.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __yadk_uid=O6dw9gVgeI3ahpyZZQK34CnjY6ut4lCb; viewed="4313213_5980585_7175434_6966737_10794370_5423775_25862578_1046265_6781800_1008074"; _vwo_uuid_v2=AF4F735B9F789D3706FCDEDAA90768C0|30f64568ffee308c04b921eb3e7e6dfd; gr_user_id=4351fb4f-6a81-4bb9-957a-3e662d8c5061; ll="108293"; _pk_ref.100001.3ac3=%5B%22%22%2C%22%22%2C1514359837%2C%22https%3A%2F%2Fwww.douban.com%2Faccounts%2Flogin%3Fredir%3Dhttps%253A%252F%252Fbook.douban.com%252Ftag%252F%2525E5%2525B0%25258F%2525E8%2525AF%2525B4%253Fstart%253D0%2526type%253DT%22%5D; ap=1; ct=y; ps=y; dbcl2="132197651:SCOhpOVGLQM"; ck=ZDrx; _pk_ses.100001.3ac3=*; __utmb=30149280.8.10.1514359852; __utmc=30149280; __utmb=81379588.8.10.1514359852; __utmc=81379588; push_noty_num=0; push_doumail_num=0; __utmt_douban=1; __utmt=1; gr_session_id_22c937bbd8ebd703f2d8e9445f7dfd03=d070604a-60d4-4aba-968a-f374557fe3ea; gr_cs1_d070604a-60d4-4aba-968a-f374557fe3ea=user_id%3A1',
-'Connection': 'keep-alive',
-'Upgrade-Insecure-Requests': 1,
-'Cache-Control': 'max-age=0'}
+'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
     
     #for item in authors:
     url = 'https://book.douban.com/author/'+str(author_id)+'/'
@@ -60,44 +56,45 @@ def get_author(author_id):
         name = soup.find(id='content').find('h1')
         if name =='':
             print('假网页，哼')
-        print('爬到了真的网页')
+        #print('爬到了真的网页')
         name = name.text
         info = soup.find(class_='info').ul.find_all('li')
-        print('get info')
+        #print('get info')
         gender = re.search(u'男|女'.encode('utf-8'),info[0].text.encode('utf-8')).group().decode('utf-8')
-        print('get gender',gender)
+        #print('get gender',gender)
         birthday = re.search(r'[0-9].*',info[1].text).group()
-        print('get birthday',birthday)
+        #print('get birthday',birthday)
         country = re.search(u'(.*):(.*)'.encode('utf-8'),info[2].text.encode('utf-8'),re.S).group(2).decode('utf-8')
-        print('get country',country)
+        #print('get country',country)
         country = country.strip()
 
 
     
         author_id = re.search(r'(.*)author/(.*)/',url).group(2)
         
-        print('get author_id',author_id)
+        #print('get author_id',author_id)
             
         out = open('author.csv', 'a', newline='')
-        print('open successfully')
+        #print('open successfully')
 
         csv_writer = csv.writer(out, dialect='excel')
-        print('csv_writer successfully')
+        #print('csv_writer successfully')
 
-        print(author_id,name,gender,birthday,country,get_booklist(author_id))
+        #print(author_id,name,gender,birthday,country,get_booklist(author_id))
 
         csv_writer.writerow([author_id,name,gender,birthday,country,get_booklist(author_id)])
         print('get_booklist successfully ')
         out.close()
     except:
         print('there is an exception')
+        time.sleep(4)
     
     
 
 if __name__=='__main__':
 
     threads = []
-    authors = open('author_id1.txt','r')
+    authors = open('authorID.txt','r')
     for item in authors:
         author_id = str(item.split()[0])
         th = threading.Thread(target=get_author,args=(author_id,))
--- get_authorID.py
+++ get_authorID.py
@@ -6,39 +6,75 @@ from bs4 import *
 import socket
 import time
 import csv
+from urllib import parse
 
-auther_ids = []
+author_ids = []
 
-def book_page(book_id):
-    global auther_ids
+def get_books(label):
+    global author_ids
+
+    books = open(label+'.txt','r')
+    for book_id in books:
+        book_id = book_id.split()[0]
+        book_page(label,book_id)
+
+
+def book_page(label,book_id):
+
+    global author_ids
     url = 'https://book.douban.com/subject/'+str(book_id)+'/'
+
     #url just like https://book.douban.com/subject/27079142/
     header = {'Host': 'book.douban.com',
 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0',
-'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-'Cookie': 'bid=9-MVpTyNO1o; __utma=30149280.1544947207.1513732889.1514340780.1514359852.7; __utmz=30149280.1513732889.1.1.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; _pk_id.100001.3ac3=bab8cd32029ce905.1514213119.6.1514362223.1514342687.; __utma=81379588.1915505992.1514213121.1514340780.1514359852.6; __utmz=81379588.1514213121.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __yadk_uid=O6dw9gVgeI3ahpyZZQK34CnjY6ut4lCb; viewed="4313213_5980585_7175434_6966737_10794370_5423775_25862578_1046265_6781800_1008074"; _vwo_uuid_v2=AF4F735B9F789D3706FCDEDAA90768C0|30f64568ffee308c04b921eb3e7e6dfd; gr_user_id=4351fb4f-6a81-4bb9-957a-3e662d8c5061; ll="108293"; _pk_ref.100001.3ac3=%5B%22%22%2C%22%22%2C1514359837%2C%22https%3A%2F%2Fwww.douban.com%2Faccounts%2Flogin%3Fredir%3Dhttps%253A%252F%252Fbook.douban.com%252Ftag%252F%2525E5%2525B0%25258F%2525E8%2525AF%2525B4%253Fstart%253D0%2526type%253DT%22%5D; ap=1; ct=y; ps=y; dbcl2="132197651:SCOhpOVGLQM"; ck=ZDrx; _pk_ses.100001.3ac3=*; __utmb=30149280.8.10.1514359852; __utmc=30149280; __utmb=81379588.8.10.1514359852; __utmc=81379588; push_noty_num=0; push_doumail_num=0; __utmt_douban=1; __utmt=1; gr_session_id_22c937bbd8ebd703f2d8e9445f7dfd03=d070604a-60d4-4aba-968a-f374557fe3ea; gr_cs1_d070604a-60d4-4aba-968a-f374557fe3ea=user_id%3A1',
-'Connection': 'keep-alive',
-'Upgrade-Insecure-Requests': 1,
-'Cache-Control': 'max-age=0'}
+'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
+
     try:
         req = urllib.request.Request(url,headers=header)
+        #print('req successfully')
         html = urllib.request.urlopen(req).read()
+        #print('html successfully')
         soup = BeautifulSoup(html,'lxml')
+        #print('soup')
         links = soup.find_all('a')
         for link in links:
+            #print('aaaaa')
             href = link.get('href')
             if re.match(r'https://book.douban.com/author/[0-9]*/',href):
+                #print('bbbb')
                 author_id = re.search(r'https://book.douban.com/author/(.*)/',href).group(1)
-                if author_id not in auther_ids:
-                    author_id_txt = open('author_id_sanwen.txt','a')
+                if author_id not in author_ids:
+                
+                    author_id_txt = open('authorID.txt','a')
+                
                     author_id_txt.write(author_id+'\n')
-                    auther_ids.append(author_id)
+                 
+                    author_id_txt.close()
+                 
+                    author_ids.append(author_id)
+              
                     print(author_id)
+                
+                else :
+                    print('已经有这个作者了')
+                break
+
     except:
         print('连接错误')
+        time.sleep(3)
 
 
 if __name__=='__main__':
-    books = open('book_sanwen.txt','r')
-    for item in books:
-        book_page(item.split()[0])
+
+
+    threads = []
+    labels = ['%E5%B0%8F%E8%AF%B4','%E5%A4%96%E5%9B%BD%E6%96%87%E5%AD%A6','%E6%BC%AB%E7%94%BB','%E6%AD%A6%E4%BE%A0','%E5%93%B2%E5%AD%A6','%E5%BF%83%E7%90%86%E5%AD%A6','%E5%84%BF%E7%AB%A5%E6%96%87%E5%AD%A6','%E9%AD%94%E5%B9%BB','%E6%82%AC%E7%96%91','%E5%90%8D%E8%91%97']
+    for label in labels:
+
+        label = parse.unquote(label)
+        print('label = ',label)
+        th = threading.Thread(target=get_books,args=(label,))
+        threads.append(th)
+    for th in threads:
+        th.start()
+        time.sleep(1)
